8e22dfe48508db646fa72b954108388bfe438c75
falztube <tubenfalz@protonmail.com>
2021-05-30 20:38:41 +0200

    wsgi

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:gunicorn huftrends.wsgi
+web:gunicorn stbabschluss.wsgi
--- stbabschluss/wsgi.py
+++ stbabschluss/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stb_abschluss.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stbabschluss.settings')
 
 application = get_wsgi_application()
